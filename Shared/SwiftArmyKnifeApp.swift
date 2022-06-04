641a0c308d007718001bb7b3825959a2ce344651
Michael Borgmann <michael@tightloop.pt>
2022-06-04 16:51:36 +0100

    Initial Commit

---
+++ Shared/SwiftArmyKnifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftArmyKnifeApp.swift
+//  Shared
+//
+//  Created by Michael Borgmann on 04/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftArmyKnifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
